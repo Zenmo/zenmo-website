4ce5aba95cae281b3e35a37f84c531ea8f9330e8
kofi-k <kofivincent026@gmail.com>
2025-04-02 15:40:24 +0000

    site: jsMain -> components - site section somposables   - Nav heaver   - footer - widgets

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/sections/Footer.kt
@@ -0,0 +1,50 @@
+package com.zenmo.web.zenmo.components.sections
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.navigation.UncoloredLinkVariant
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.toAttrs
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.style.vars.color.ColorVar
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.zenmo.web.zenmo.SubheadlineTextStyle
+import com.zenmo.web.zenmo.toSitePalette
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
+
+val FooterStyle = CssStyle.base {
+    Modifier
+        .backgroundColor(colorMode.toSitePalette().nearBackground)
+        .padding(topBottom = 1.5.cssRem, leftRight = 10.percent)
+}
+
+@Composable
+fun Footer(modifier: Modifier = Modifier) {
+    val sitePalette = ColorMode.current.toSitePalette()
+    Row(
+        FooterStyle.toModifier().then(modifier),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Div(SubheadlineTextStyle.toAttrs()) {
+            Text(value = "©2018 ZEnMo Simulations BV")
+        }
+        Div(SubheadlineTextStyle.toAttrs()) {
+            Link(
+                "https://zenmo.com/",
+                "Disclaimer",
+                Modifier.setVariable(ColorVar, sitePalette.brand.primary),
+                variant = UncoloredLinkVariant
+            )
+        }
+    }
+}
