009247b11e95aa9a836285bafb83d790bc61fd60
Kofi K <kofivincent026@gmail.com>
2025-04-04 09:53:31 +0000

    Site module  (#17)
    Create Kobweb site

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/pages/Index.kt
@@ -0,0 +1,83 @@
+package com.zenmo.web.zenmo.pages
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.StyleVariable
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.style.toAttrs
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.zenmo.web.zenmo.HeadlineTextStyle
+import com.zenmo.web.zenmo.components.layouts.PageLayout
+import com.zenmo.web.zenmo.toSitePalette
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.vh
+import org.jetbrains.compose.web.dom.Div
+
+// Container that has a tagline and grid on desktop, and just the tagline on mobile
+val HeroContainerStyle = CssStyle {
+    base { Modifier.fillMaxWidth().gap(2.cssRem) }
+    Breakpoint.MD { Modifier.margin { top(20.vh) } }
+}
+
+// A demo grid that appears on the homepage because it looks good
+val HomeGridStyle = CssStyle.base {
+    Modifier
+        .gap(0.5.cssRem)
+        .width(70.cssRem)
+        .height(18.cssRem)
+}
+
+private val GridCellColorVar by StyleVariable<Color>()
+val HomeGridCellStyle = CssStyle.base {
+    Modifier
+        .backgroundColor(GridCellColorVar.value())
+        .boxShadow(blurRadius = 0.6.cssRem, color = GridCellColorVar.value())
+        .borderRadius(1.cssRem)
+}
+
+@Composable
+private fun GridCell(color: Color, row: Int, column: Int, width: Int? = null, height: Int? = null) {
+    Div(
+        HomeGridCellStyle.toModifier()
+            .setVariable(GridCellColorVar, color)
+            .gridItem(row, column, width, height)
+            .toAttrs()
+    )
+}
+
+@Page
+@Composable
+fun HomePage() {
+    PageLayout("Home") {
+        val sitePalette = ColorMode.current.toSitePalette()
+        Column(
+            modifier = HeroContainerStyle.toModifier(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Div(HeadlineTextStyle.toAttrs()) {
+                SpanText("Hello ")
+                SpanText(
+                    "ZEnMo \\(*.*)/",
+                    Modifier
+                        .color(sitePalette.brand.accent)
+                        // Use a shadow so this light-colored word is more visible in light mode
+                        .textShadow(0.px, 0.px, blurRadius = 0.5.cssRem, color = Colors.Gray)
+                )
+            }
+        }
+    }
+}
