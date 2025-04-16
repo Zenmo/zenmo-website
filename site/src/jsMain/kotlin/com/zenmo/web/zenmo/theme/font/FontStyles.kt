a570c30db808008637ae2086b8041c31a498d87f
kofi-k <kofivincent026@gmail.com>
2025-04-16 16:33:37 +0000

    site: jsMain: theme -> defined responsive font styles and centralized font configurations

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/theme/font/FontStyles.kt
@@ -0,0 +1,115 @@
+package com.zenmo.web.zenmo.theme.font
+
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.font
+import com.varabyte.kobweb.silk.style.ComponentKind
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.addVariant
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.zenmo.web.zenmo.theme.SitePalettes
+
+sealed interface TextComponentKind : ComponentKind
+
+val TextStyle = CssStyle<TextComponentKind> {
+    val defaultFonts = Fonts[Breakpoint.LG]
+    base {
+        Modifier.font { defaultFonts.bodyLarge }
+    }
+}
+
+val TextStylePrimaryColor = TextStyle.addVariant {
+    base {
+        Modifier.color(SitePalettes.light.brand.onBackground)
+    }
+}
+
+val TextStyleSecondaryColor = TextStyle.addVariant {
+    base {
+        Modifier.color(SitePalettes.light.brand.secondary)
+    }
+}
+
+val TextStyleBrandColor = TextStyle.addVariant {
+    base {
+        Modifier.color(SitePalettes.light.brand.primary)
+    }
+}
+
+
+val DisplayTextStyle = TextStyle.addVariant {
+    Breakpoint.ZERO {
+        val fontsSM = Fonts[Breakpoint.SM]
+        Modifier.font { siteFont(fontsSM.display) }
+    }
+    Breakpoint.SM {
+        val fontsSM = Fonts[Breakpoint.SM]
+        Modifier.font { siteFont(fontsSM.display) }
+    }
+    Breakpoint.MD {
+        val fontsMD = Fonts[Breakpoint.MD]
+        Modifier.font { siteFont(fontsMD.display) }
+    }
+    Breakpoint.LG {
+        val fontsLG = Fonts[Breakpoint.LG]
+        Modifier.font { siteFont(fontsLG.display) }
+    }
+    Breakpoint.XL {
+        val fontsXL = Fonts[Breakpoint.XL]
+        Modifier.font { siteFont(fontsXL.display) }
+    }
+}
+
+
+val BodyLargeTextStyle = TextStyle.addVariant {
+    Breakpoint.ZERO {
+        val fontsSM = Fonts[Breakpoint.SM]
+        Modifier.font { siteFont(fontsSM.bodyLarge) }
+    }
+    Breakpoint.SM {
+        val fontsSM = Fonts[Breakpoint.SM]
+        Modifier.font { siteFont(fontsSM.bodyLarge) }
+    }
+    Breakpoint.MD {
+        val fontsMD = Fonts[Breakpoint.MD]
+        Modifier.font { siteFont(fontsMD.bodyLarge) }
+    }
+    Breakpoint.LG {
+        val fontsLG = Fonts[Breakpoint.LG]
+        Modifier.font { siteFont(fontsLG.bodyLarge) }
+    }
+    Breakpoint.XL {
+        val fontsXL = Fonts[Breakpoint.XL]
+        Modifier.font { siteFont(fontsXL.bodyLarge) }
+    }
+}
+
+
+val LabelLargeTextStyle = TextStyle.addVariant {
+    Breakpoint.ZERO {
+        val fontsSM = Fonts[Breakpoint.SM]
+        Modifier.font { siteFont(fontsSM.labelLarge) }
+    }
+    Breakpoint.SM {
+        val fontsSM = Fonts[Breakpoint.SM]
+        Modifier.font { siteFont(fontsSM.labelLarge) }
+    }
+    Breakpoint.MD {
+        val fontsMD = Fonts[Breakpoint.MD]
+        Modifier.font { siteFont(fontsMD.labelLarge) }
+    }
+    Breakpoint.LG {
+        val fontsLG = Fonts[Breakpoint.LG]
+        Modifier.font { siteFont(fontsLG.labelLarge) }
+    }
+    Breakpoint.XL {
+        val fontsXL = Fonts[Breakpoint.XL]
+        Modifier.font { siteFont(fontsXL.labelLarge) }
+    }
+}
+
+
+/*todo add style variants for
+*  1.TitleTextStyle
+*  2. LabelMediumTextStyle
+*  3. BodyMediumTextStyle & BodySmall maybe  */
