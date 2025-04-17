21c6827781f75e2a547a079c7935a63346d5a3e7
kofi-k <kofivincent026@gmail.com>
2025-04-17 05:34:42 +0000

    site: jsMain: localization-services: - refactoring unneeded implementations

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/core/services/localization/Localization.kt
+++
@@ -1,7 +0,0 @@
-package com.zenmo.web.zenmo.core.services.localization
-
-import com.zenmo.web.zenmo.core.data.StringRes
-
-interface Localization {
-    fun getLocalizedString(resValue: StringRes, locale: Local): String
-}
