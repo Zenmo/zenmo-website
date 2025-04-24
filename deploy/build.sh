83da1b86407353aee8886cf71eea978f789bdf6a
Kofi K <kofivincent026@gmail.com>
2025-04-24 13:55:04 +0000

    Switch docker build process to JDK. (#30)
    * Switch docker build process to JDK.
    
    ---------
    
    Co-authored-by: Erik van Velzen <erik@evanv.nl>

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
 
 set -ex
 
 source $(dirname "$0")/vars.sh
 
+cd $(dirname "$0")/..
+
 docker build \
     --file deploy/Dockerfile \
     --tag $TAG \
     --progress=plain \
-    --push \
     .
