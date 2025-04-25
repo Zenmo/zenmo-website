a565b889652992f3044adc5c48df974b41c4af1d
Erik van Velzen <erik@evanv.nl>
2025-04-25 10:05:55 +0200

    Enable docker cache mount in GitHub Actions (#35)

--- deploy/build.sh
+++ deploy/build.sh
@@ -2,12 +2,17 @@
 
 set -ex
 
-source $(dirname "$0")/vars.sh
-
 cd $(dirname "$0")/..
 
-docker build \
+source $(dirname "$0")/vars.sh
+
+# It seems we can always specify GitHub Actions caching
+# and if it's not available it will fall back to local caching.
+docker buildx build \
     --file deploy/Dockerfile \
     --tag $TAG \
     --progress=plain \
+    --cache-to type=gha,mode=max,scope=frontend \
+    --cache-from type=gha,scope=frontend \
+    --push \
     .
