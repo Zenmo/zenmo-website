316bb9c728569553016bf08aae45744e5456db29
kofi-k <kofivincent026@gmail.com>
2025-04-29 17:14:00 +0000

    Refactor PageLayout with Box container and grid tweaks.

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/layouts/PageLayout.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/layouts/PageLayout.kt
@@ -2,16 +2,17 @@ package com.zenmo.web.zenmo.components.layouts
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.ColumnScope
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.gridRow
+import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.zenmo.web.zenmo.components.sections.Footer
 import com.zenmo.web.zenmo.components.sections.nav_header.NavHeader
 import kotlinx.browser.document
+import org.jetbrains.compose.web.css.fr
+import org.jetbrains.compose.web.css.percent
 
 
 @Composable
@@ -19,12 +20,20 @@ fun PageLayout(title: String, content: @Composable ColumnScope.() -> Unit) {
     LaunchedEffect(title) {
         document.title = "Zenmo - $title"
     }
-    Column(
-        Modifier.fillMaxSize().gridRow(1),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .minHeight(100.percent)
+            .gridTemplateRows { size(1.fr); size(minContent) },
+        contentAlignment = Alignment.Center
     ) {
-        NavHeader()
-        content()
+        Column(
+            Modifier.fillMaxSize().gridRow(1),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            NavHeader()
+            content()
+        }
+        Footer(Modifier.fillMaxWidth().gridRow(2))
     }
-    Footer(Modifier.fillMaxWidth().gridRow(2))
 }
