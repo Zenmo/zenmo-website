320d3679848d60d629d803666e63e7e10a018757
Kofi K <kofivincent026@gmail.com>
2025-05-09 07:08:24 +0000

    Menu Pages and shiny new Zenmo logo (#57)
    Co-authored-by: Iris <iris@zenmo.com>

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/pages/WhatWeDo.kt
@@ -0,0 +1,42 @@
+package com.zenmo.web.zenmo.pages
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.style.toModifier
+import com.zenmo.web.zenmo.components.layouts.PageLayout
+import com.zenmo.web.zenmo.theme.font.DisplayTextStyle
+import com.zenmo.web.zenmo.theme.font.TextStyle
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
+
+@Page
+@Composable
+fun WhatPage() {
+    PageLayout("What we do") {
+        Column(
+            modifier = HeroContainerStyle.toModifier(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Div(TextStyle.toModifier(DisplayTextStyle).toAttrs()) {
+                SpanText("We do very interesting things.")
+            }
+
+            val ctx = rememberPageContext()
+            Button(
+                onClick = {
+                    ctx.router.tryRoutingTo("/")
+                },
+            ) {
+                Text("Go back to the homepage")
+            }
+        }
+    }
+}
