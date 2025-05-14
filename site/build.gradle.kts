6f0c63a5b03f0e4b122bbc7c75d67ac46eedda9b
kofi-k <kofivincent026@gmail.com>
2025-05-14 05:21:57 +0000

    Add Kotlin test setup and LanguageManagerTest

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -32,9 +32,6 @@ kotlin {
     configAsKobwebApplication("zenmo" /*, includeServer = true*/)
 
     sourceSets {
-//        commonMain.dependencies {
-//          // Add shared dependencies between JS and JVM here if building a fullstack app
-//        }
         all {
             languageSettings.optIn("kotlin.uuid.ExperimentalUuidApi")
         }
@@ -45,15 +42,14 @@ kotlin {
             implementation(libs.compose.html.core)
             implementation(libs.kobweb.core)
             implementation(libs.kobweb.silk)
-            // This default template uses built-in SVG icons, but what's available is limited.
-            // Uncomment the following if you want access to a large set of font-awesome icons:
             implementation(libs.silk.icons.mdi)
             implementation(libs.kobwebx.markdown)
         }
 
-        // Uncomment the following if you pass `includeServer = true` into the `configAsKobwebApplication` call.
-//        jvmMain.dependencies {
-//            compileOnly(libs.kobweb.api) // Provided by Kobweb backend at runtime
-//        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
     }
 }
