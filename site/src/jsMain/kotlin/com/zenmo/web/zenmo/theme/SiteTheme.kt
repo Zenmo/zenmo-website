dfcf9eb47cc6d5f96142aa1fc647a4e08d490aca
kofi-k <kofivincent026@gmail.com>
2025-05-30 07:52:11 +0000

    Add Lux-specific color hues and data visualization colors to SitePalette

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/theme/SiteTheme.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/theme/SiteTheme.kt
@@ -11,6 +11,14 @@ import com.varabyte.kobweb.silk.theme.colors.palette.overlay
 import com.zenmo.web.zenmo.pages.SiteGlobals
 import kotlinx.browser.window
 
+
+data class LuxSpecificColorHues(
+    val luxBlue: Color = Color.rgb(0x3C83EF),
+    val luxRed: Color = Color.rgb(0xF8285A),
+    val luxYellow: Color = Color.rgb(0xFFC300),
+    val luxGreen: Color = Color.rgb(0x28A745),
+)
+
 data class SitePalette(
     val primary: Color = Color.rgb(0xb6084e),
     val onPrimary: Color = Color.rgb(0xFFFFFF),
@@ -18,20 +26,44 @@ data class SitePalette(
     val secondary: Color = Color.rgb(0x3d2c2f),
     val surfaceContainerLow: Color = Color.rgb(0xFFF0F1),
     val surface: Color = Color.rgb(0xFFF8F7),
-    val onBackground: Color = Color.rgb(0x26181A)
+    val onBackground: Color = Color.rgb(0x26181A),
+
+    // Lux data visualization Colors for map legends and charts.
+    val luxDataVizColors: List<Color> = emptyList(),
+    val luxSpecificColorHues: LuxSpecificColorHues = LuxSpecificColorHues()
+
+    /** TODO: Standardized Colors for Specific Concepts/Purposes
+     *    like electricity, gasoline, etc.
+     *
+     *   val electricity: Color,
+     *   val gasoline: Color,
+     */
 ) {
     companion object {
         private val zenmoColorPalette = SitePalette()
 
-        //just pilot colors till we have a proper palette for lux
         private val luxColorPalette = SitePalette(
-            primary = Color.rgb(0x3C83EF),
+            primary = Color.rgb(0x004548), // dark teal
             onPrimary = Color.rgb(0xFFFFFF),
             primaryContainer = Color.rgb(0xd7e2ff),
-            secondary = Color.rgb(0x495e88),
-            surfaceContainerLow = Color.rgb(0xf2f3fd),
-            surface = Color.rgb(0xf9f9ff),
-            onBackground = Color.rgb(0x191c22)
+            secondary = Color.rgb(0xffc300), // orange-ish yellow
+            surfaceContainerLow = Color.rgb(0xf2f4f4),
+            surface = Color.rgb(0xf7faf9),
+            onBackground = Color.rgb(0x2d3131),
+
+            /** generated these with https://material-foundation.github.io/material-theme-builder/
+             * using the Lux color palette as a base
+             */
+            luxDataVizColors = listOf(
+                Color.rgb(0xFFC300), // orange-ish yellow like secondary
+                Color.rgb(0x007E84),  // teal, not dark teal
+                Color.rgb(0x8bd2d7), // light teal, an inverse of the teal
+                Color.rgb(0xffe5b2), // medium contrast of the orange-yellow
+                Color.rgb(0x3C83EF), // blue
+                Color.rgb(0xF8285A), // red
+            ),
+
+            luxSpecificColorHues = LuxSpecificColorHues()
         )
         val light = when (window.location.host) {
             SiteGlobals.ZENMO_DOMAIN -> zenmoColorPalette
