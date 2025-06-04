9b1113ae34701fec4cf6cd3672f2db40f30db422
kofi-k <kofivincent026@gmail.com>
2025-06-04 15:25:03 +0000

    a little stying on card links (#90)

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/widgets/CardLink.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/widgets/CardLink.kt
@@ -1,27 +1,22 @@
 package com.zenmo.web.zenmo.components.widgets
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.css.Cursor
 import com.varabyte.kobweb.compose.css.ObjectFit
 import com.varabyte.kobweb.compose.css.TextDecorationLine
 import com.varabyte.kobweb.compose.css.Transition
+import com.varabyte.kobweb.compose.css.TransitionTimingFunction
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.lightened
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
-import com.varabyte.kobweb.core.rememberPageContext
 import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.StyleScope
 import com.varabyte.kobweb.silk.style.selectors.hover
 import com.varabyte.kobweb.silk.style.toModifier
 import com.zenmo.web.zenmo.domains.lux.widgets.headings.SubHeaderText
 import com.zenmo.web.zenmo.theme.SitePalette
-import org.jetbrains.compose.web.css.Color
-import org.jetbrains.compose.web.css.cssRem
-import org.jetbrains.compose.web.css.ms
-import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Img
 import org.jetbrains.compose.web.dom.P
@@ -31,49 +26,27 @@ val CardLinkStyle = CssStyle {
         Modifier
             .borderRadius(24.px)
             .background(Color.white)
+            .boxShadow(0.px, 2.px, 4.px, 0.px, rgba(0, 0, 0, 0.24f))
+            .transition(Transition.group(setOf("box-shadow", "transform"), 350.ms, TransitionTimingFunction.Ease))
     }
-    hover {
+    cssRule(":is(:hover, :focus)") {
         Modifier
-            .cursor(Cursor.Pointer)
-            .boxShadow(
-                offsetX = 0.px,
-                offsetY = 4.px,
-                blurRadius = 8.px,
-                spreadRadius = 0.px,
-                color = Color("#00000033")
-            )
+            .boxShadow(0.px, 8.px, 10.px, 1.px, rgba(0, 0, 0, 0.2f))
+            .transform { translateY((-8).px) }
     }
 
     cssRule("> div > .cardImage") {
         Modifier
             .fillMaxWidth()
             .height(140.px)
-            .borderRadius(24.px)
-            .objectFit(ObjectFit.Cover)
-            .padding(leftRight = 0.5.cssRem, top = 0.5.cssRem)
-    }
-
-    cssRule(":hover > div > .cardImage") {
-        Modifier
             .borderRadius(topRight = 24.px, topLeft = 24.px)
-            .padding(0.cssRem)
-            .transition(
-                Transition.of(
-                    "border-radius",
-                    200.ms,
-                ),
-
-                Transition.of(
-                    "padding",
-                    200.ms,
-                ),
-            )
+            .objectFit(ObjectFit.Cover)
     }
 }
 
 val LinkNoStyle = CssStyle {
     base {
-        Modifier.color(Color.black)
+        Modifier.color(SitePalette.light.onBackground)
     }
     hover {
         Modifier.textDecorationLine(TextDecorationLine.None)
